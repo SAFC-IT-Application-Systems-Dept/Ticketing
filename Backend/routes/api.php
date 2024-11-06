6a79f840bfb37cf60a226a5dc25e94f4aea93dfa
Emmanuel Joshua A. Lemon <emmanuel.lemon@safc.com.ph>
2024-11-06 14:47:32 +0800

    backend

---
+++ Backend/routes/api.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Maintenance\BranchController;
+use App\Http\Controllers\Maintenance\DepartmentController;
+use App\Http\Controllers\Maintenance\SectionController;
+use App\Http\Controllers\Maintenance\CategoryController;
+use App\Http\Controllers\Maintenance\UserController;
+use App\Http\Controllers\Ticket\TicketHdrController;
+use App\Http\Controllers\Ticket\TicketLogController;
+use App\Http\Controllers\testController;
+
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('dashboard', DashboardController::class);
+    Route::get('get-user', UserController::class);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware(['auth'])->group(function () {
+    //maintenance routes
+    Route::prefix('maintenance')->group(function () {
+        Route::resource('branch', BranchController::class);
+        Route::resource('department', DepartmentController::class);
+        Route::resource('section', SectionController::class);
+        Route::resource('category', CategoryController::class);
+    });
+
+    //tickets routes
+    Route::prefix('ticket')->group(function () {
+        Route::get('ticket-logs', TicketLogController::class);
+        Route::resource('ticket-hdr', TicketHdrController::class);
+    });
+});
+
+
+
